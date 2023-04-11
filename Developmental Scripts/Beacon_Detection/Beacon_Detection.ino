86c05126fabec775ad952cd6bdf8b7cf5c4bb26d
BloopingMinotaur <kohlernoah@gmail.com>
2023-04-11 10:13:21 -0500

    updated beacon detect scripts

--- Developmental Scripts/Beacon_Detection/Beacon_Detection.ino
+++ Developmental Scripts/Beacon_Detection/Beacon_Detection.ino
@@ -3,9 +3,9 @@
 // IR Beacon Initialization Values
 #define green GREEN_LED
 #define red RED_LED
-int IRbeaconLeft = 15;
+int IRbeaconLeft = 14;
 int IRStateLeft = 1;
-int IRbeaconMid = 16;
+int IRbeaconMid = 18;
 int IRStateMid = 1;
 int IRbeaconRight = 17;
 int IRStateRight = 1;
@@ -17,29 +17,6 @@ int midBeaconCounter = 0;
 int rightBeaconCounter = 0;
 uint16_t LED_ShootingRight = 9;
 
-
-
-uint16_t rightSpeed = 5;
-uint16_t leftSpeed = 5;
-uint16_t normalEncSpeed = 4;
-uint16_t fastEncSpeed = 6;
-uint32_t left_enc_curr;
-uint32_t right_enc_curr;
-uint32_t left_enc_prev;
-uint32_t right_enc_prev;
-uint32_t left_enc_speed;
-uint32_t right_enc_speed;
-uint32_t leftEncTurn;
-uint32_t rightEncTurn;
-int encoder_t = 15;
-int prev_t;
-bool calibrated = false;
-bool leftCorrect = false;
-int correctT = 0;
-int turningT = 0;
-int startT = 0;
-bool turnLeft = false;
-bool turnRight = false;
 
 uint16_t sensorVal[LS_NUM_SENSORS];
 uint16_t sensorCalVal[LS_NUM_SENSORS];
@@ -58,208 +35,79 @@ enum decisionStates {
 };
 
 decisionStates currDecState = DETECTING;
-decisionStates prevDecState = DETECTING;
-
-void ninety_right() {  // needs to be tuned for a 90 degree turn through either time or encoder
-  getMotorSpeed(normalEncSpeed, normalEncSpeed);
-  if ((getEncoderLeftCnt() - leftEncTurn) < 180) {
-    enableMotor(LEFT_MOTOR);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(LEFT_MOTOR, leftSpeed);
-  } else {
-    disableMotor(LEFT_MOTOR);
-  }
-  if ((getEncoderRightCnt() - rightEncTurn) < 180) {
-    enableMotor(RIGHT_MOTOR);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorSpeed(RIGHT_MOTOR, rightSpeed);
-  } else {
-    disableMotor(RIGHT_MOTOR);
-  }
-  if (((getEncoderRightCnt() - rightEncTurn) > 180) && ((getEncoderLeftCnt() - leftEncTurn) > 180)) {
-    turnRight = false;
-  }
-}
-
-void ninety_left() {  // needs to be tuned for a 90 degree turn through either time or encoder
-  getMotorSpeed(normalEncSpeed, normalEncSpeed);
-  if ((getEncoderLeftCnt() - leftEncTurn) < 180) {
-    enableMotor(LEFT_MOTOR);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorSpeed(LEFT_MOTOR, leftSpeed);
-  } else {
-    disableMotor(LEFT_MOTOR);
-  }
-  if ((getEncoderRightCnt() - rightEncTurn) < 180) {
-    enableMotor(RIGHT_MOTOR);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(RIGHT_MOTOR, rightSpeed);
-  } else {
-    disableMotor(RIGHT_MOTOR);
-  }
-  if (((getEncoderRightCnt() - rightEncTurn) > 180) && ((getEncoderLeftCnt() - leftEncTurn) > 180)) {
-    turnLeft = false;
-  }
-  Serial.println(getEncoderLeftCnt() - leftEncTurn);
-  Serial.println(getEncoderRightCnt() - rightEncTurn);
-}
-
-void face_left_basket() {
-  getMotorSpeed(normalEncSpeed, normalEncSpeed);
-  if ((getEncoderLeftCnt() - leftEncTurn) < 90) {
-    enableMotor(LEFT_MOTOR);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorSpeed(LEFT_MOTOR, leftSpeed);
-  } else {
-    disableMotor(LEFT_MOTOR);
-  }
-  if ((getEncoderRightCnt() - rightEncTurn) < 90) {
-    enableMotor(RIGHT_MOTOR);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(RIGHT_MOTOR, rightSpeed);
-  } else {
-    disableMotor(RIGHT_MOTOR);
-  }
-  if (((getEncoderRightCnt() - rightEncTurn) > 180) && ((getEncoderLeftCnt() - leftEncTurn) > 180)) {
-    turnLeft = false;
-  }
-  Serial.println(getEncoderLeftCnt() - leftEncTurn);
-  Serial.println(getEncoderRightCnt() - rightEncTurn);
-}
-
-void face_right_basket() {
-  getMotorSpeed(normalEncSpeed, normalEncSpeed);
-  if ((getEncoderLeftCnt() - leftEncTurn) < 90) {
-    enableMotor(LEFT_MOTOR);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(LEFT_MOTOR, leftSpeed);
-  } else {
-    disableMotor(LEFT_MOTOR);
-  }
-
-  if ((getEncoderRightCnt() - rightEncTurn) < 90) {
-    enableMotor(RIGHT_MOTOR);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorSpeed(RIGHT_MOTOR, rightSpeed);
-  } else {
-    disableMotor(RIGHT_MOTOR);
-  }
-  if (((getEncoderRightCnt() - rightEncTurn) > 90) && ((getEncoderLeftCnt() - leftEncTurn) > 133)) {
-    turnRight = false;
-  }
-}
-
-void stopper() {
-  disableMotor(BOTH_MOTORS);
-}
-
-void turnTest() {
-  int start = millis();
-  int curr = millis();
-  int rightEncStart = getEncoderRightCnt();
-  int leftEncStart = getEncoderLeftCnt();
-  while ((curr - start) < 700) {
-    getMotorSpeed(normalEncSpeed, normalEncSpeed);
-    enableMotor(BOTH_MOTORS);
-    setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
-    setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
-    setMotorSpeed(RIGHT_MOTOR, rightSpeed);
-    setMotorSpeed(LEFT_MOTOR, leftSpeed);
-    curr = millis();
-  }
-  Serial.println(getEncoderLeftCnt() - leftEncStart);
-  Serial.println(getEncoderRightCnt() - rightEncStart);
-}
-
-void getMotorSpeed(uint16_t leftEncSpeed, uint16_t rightEncSpeed) {
-  if ((millis() - prev_t) > encoder_t) {
-    left_enc_curr = getEncoderLeftCnt();
-    right_enc_curr = getEncoderRightCnt();
-    left_enc_speed = left_enc_curr - left_enc_prev;
-    right_enc_speed = right_enc_curr - right_enc_prev;
-
-    prev_t = millis();
-    left_enc_prev = left_enc_curr;
-    right_enc_prev = right_enc_curr;
-
-    if (left_enc_speed < leftEncSpeed) {
-      leftSpeed = leftSpeed + 1;
-    } else if (left_enc_speed > leftEncSpeed) {
-      leftSpeed = leftSpeed - 1;
-    }
-    if (right_enc_speed < rightEncSpeed) {
-      rightSpeed = rightSpeed + 1;
-    } else if (right_enc_speed > rightEncSpeed) {
-      rightSpeed = rightSpeed - 1;
-    }
-  }
-}
+decisionStates prevDecState = STARTUP;
 
 void detecting() {
-  if (prevDecState != currDecState) {
-    detectStartTime = millis();
-    detectEndTime = detectStartTime + 1000;
-    leftBeaconCounter = 0;
-    midBeaconCounter = 0;
-    rightBeaconCounter = 0;
-    digitalWrite(LED_ShootingRight, LOW);
-    digitalWrite(red, LOW);
-    digitalWrite(green, LOW);
+  Serial.println("detecting");
+  if (currDecState == DETECTING) {
+    if (prevDecState != currDecState) {
+      detectStartTime = millis();
+      detectEndTime = detectStartTime + 5000;
+      leftBeaconCounter = 0;
+      midBeaconCounter = 0;
+      rightBeaconCounter = 0;
+      digitalWrite(LED_ShootingRight, LOW);
+      digitalWrite(red, LOW);
+      digitalWrite(green, LOW);
+    }
+    if (millis() < detectEndTime) {
+      Serial.println("reading");
+      IRStateLeft = digitalRead(IRbeaconLeft);
+      IRStateMid = digitalRead(IRbeaconMid);
+      IRStateRight = digitalRead(IRbeaconRight);
+
+      if (IRStateLeft == 0) {
+        leftBeaconCounter++;
+        Serial.print("leftBeaconCounter:");
+        Serial.println(leftBeaconCounter);
+      }
+      if (IRStateMid == 0) {
+        midBeaconCounter++;
+        Serial.print("midBeaconCounter:");
+        Serial.println(midBeaconCounter);
+      }
+      if (IRStateRight == 0) {
+        rightBeaconCounter++;
+        Serial.print("rightBeaconCounter:");
+        Serial.println(rightBeaconCounter);
+      }
+
+      delay(1);
+
+
+    } else {
+      //set new currDecState Variable here
+      if (leftBeaconCounter > midBeaconCounter && leftBeaconCounter > rightBeaconCounter) {
+        currDecState = SHOOTING_LEFT;
+        digitalWrite(red, HIGH);
+        Serial.println("shoot left");
+      } else if (midBeaconCounter > leftBeaconCounter && midBeaconCounter > rightBeaconCounter) {
+        currDecState = SHOOTING_MID;
+        digitalWrite(green, HIGH);
+        Serial.println("shoot mid");
+      } else if (rightBeaconCounter > leftBeaconCounter && rightBeaconCounter > midBeaconCounter) {
+        currDecState = SHOOTING_RIGHT;
+        digitalWrite(LED_ShootingRight, HIGH);
+        Serial.println("shoot right");
+      }
+    }
+
   }
-  if (millis() < detectEndTime) {
-    IRStateLeft = digitalRead(IRbeaconLeft);
-    IRStateMid = digitalRead(IRbeaconMid);
-    IRStateRight = digitalRead(IRbeaconRight);
-
-    if (IRStateLeft == 0) {
-      leftBeaconCounter++;
-    }
-    if (IRStateMid == 0) {
-      midBeaconCounter++;
-    }
-    if (IRStateRight == 0) {
-      rightBeaconCounter++;
-    }
-
-    delay(1);
-
-
-  } else {
-    //set new currDecState Variable here
-    if (leftBeaconCounter > midBeaconCounter && leftBeaconCounter > rightBeaconCounter) {
-      currDecState = SHOOTING_LEFT;
-      digitalWrite(red, HIGH);
-    } else if (midBeaconCounter > leftBeaconCounter && midBeaconCounter > rightBeaconCounter) {
-      currDecState = SHOOTING_MID;
-      digitalWrite(green, HIGH);
-    } else if (rightBeaconCounter > leftBeaconCounter && rightBeaconCounter > midBeaconCounter) {
-      currDecState = SHOOTING_RIGHT;
-      digitalWrite(LED_ShootingRight, HIGH);
-    }
-  }
-
 }
-
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   setupRSLK();
-  setupEncoder(72, 12, 56, 13);
-  prev_t = millis();
-
-  setupWaitBtn(LP_LEFT_BTN);
-  /* Red led in rgb led */
   setupLed(RED_LED);
   setupLed(GREEN_LED);
-  clearMinMax(sensorMinVal, sensorMaxVal);
   pinMode(IRbeaconLeft, INPUT_PULLUP);  // NOTE: because this is a pullup, a 1 indicates no beacon detected, 0 is yes beacon detected
   pinMode(IRbeaconMid, INPUT_PULLUP);
   pinMode(IRbeaconRight, INPUT_PULLUP);
 }
 
-int counter = 0;
 
 void loop() {
   detecting();
+  prevDecState = currDecState;
 
 }
